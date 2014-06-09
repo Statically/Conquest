6d6c2d5cdbf222d7ee61e0f7ab249b7248d79dd1
Blackveiled (Adam Canfield) <>
2014-06-10 00:25:09 +0200

    Initial commit

---
+++ src/com/blackveiled/plugins/conquest/Conquest.java
@@ -0,0 +1,29 @@
+package com.blackveiled.plugins.conquest;
+
+// Imports
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Blackveiled
+ * @author thedarklord197
+ *
+ */
+public class Conquest extends JavaPlugin {
+
+    public static byte GAME_MODE = 0x00; // default game mode: Conquest
+
+    // On Enable
+    @Override
+    public void onEnable() {
+        getConfig();
+        saveDefaultConfig();
+    }
+
+    // On Disable
+    @Override
+    public void onDisable() {
+
+    }
+
+}
