e126c85d372dbcfd04fc35d20a36fff460080038
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2014-06-11 18:20:27 +0100

    Added my events
    events directory created, EmpireCollapseEvent implemented

---
+++ src/com/Statically/conquest/events/EmpireCollapseEvent.java
@@ -0,0 +1,48 @@
+package com.blackveiled.plugins.conquest.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import com.blackveiled.plugins.conquest.empires.Empire;
+
+/**
+ * 
+ * @author Blackveiled
+ * @author thedarklord197
+ * 
+ * */
+
+public class EmpireCollapseEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+	
+	private Empire empire;
+	private Empire empire2;
+	
+	/* 
+	 * Event triggered when an empire collapses
+	 * getEmpire retrieves the fallen empire
+	 * getOtherEmpire retrieves the empire which conquered empire
+	 * */
+	
+    public EmpireCollapseEvent(Empire empire, Empire empire2) {
+    	this.empire = empire;
+    	this.empire2 = empire2;
+    }
+    
+    public Empire getEmpire() {
+    	return empire;
+    }
+    
+    public Empire getCause() {
+    	return empire2;
+    }
+	
+	public HandlerList getHandlers() {
+	    return handlers;
+	}
+	 
+	public static HandlerList getHandlerList() {
+	    return handlers;
+	}
+
+}
